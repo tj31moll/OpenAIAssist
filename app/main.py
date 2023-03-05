d96ccaa65c404e3e4cd882c1f86062f06286f81e
tj31moll <91799649+tj31moll@users.noreply.github.com>
2023-03-05 17:39:19 -0500

    Update main.py

--- app/main.py
+++ app/main.py
@@ -4,14 +4,19 @@ import telegram
 import speech_recognition as sr
 import pyttsx3
 from chatterbot import ChatBot
-from chatterbot.trainers import ChatterBotCorpusTrainerimport
+from chatterbot.trainers import ChatterBotCorpusTrainer
 import requests
+from google.oauth2.credentials import Credentials
+from google.assistant.library import Assistant
+from google.assistant.library.event import EventType
 
 # Define environment variables
 openai.api_key = os.environ["OPENAI_API_KEY"]
 telegram_token = os.environ["TELEGRAM_TOKEN"]
 telegram_chat_id = os.environ["TELEGRAM_CHAT_ID"]
 zapier_webhook_url = os.environ["ZAPIER_WEBHOOK_URL"]
+google_project_id = os.environ["GOOGLE_PROJECT_ID"]
+google_credentials = os.environ["GOOGLE_APPLICATION_CREDENTIALS"]
 
 # Initialize ChatBot and train with corpus
 chatbot = ChatBot('MyChatBot')
@@ -25,30 +30,43 @@ bot = telegram.Bot(token=telegram_token)
 
 def main():
     log_file = open("conversation_log.txt", "a")
-    while True:
-        user_input = listen()
-        log_file.write("User: " + user_input + "\n")
-        if user_input.lower() == 'bye':
-            speak("Goodbye!")
-            log_file.write("Assistant: Goodbye!\n")
-            break
-        elif user_input.lower().startswith('hey google'):
-            send_to_zapier(user_input)
-        elif user_input.lower().startswith('chatbot'):
-            response = chatbot.get_response(user_input[7:])
-            send_to_telegram(response)
-            speak(response)
-            log_file.write("Assistant: " + str(response) + "\n")
-        elif user_input.lower().startswith('openai'):
-            response = generate_response(user_input[7:])
-            send_to_telegram(response)
-            speak(response)
-            log_file.write("Assistant: " + str(response) + "\n")
-        else:
-            response = chatbot.get_response(user_input)
-            send_to_telegram(response)
-            speak(response)
-            log_file.write("Assistant: " + str(response) + "\n")
+    with Assistant(credentials=Credentials.from_authorized_user_info(info=None), project_id=google_project_id) as assistant:
+        for event in assistant.start():
+            if event.type == EventType.ON_START_FINISHED:
+                log_file.write("Assistant: Ready!\n")
+                print('Assistant ready')
+            elif event.type == EventType.ON_CONVERSATION_TURN_STARTED:
+                print('Assistant listening')
+            elif event.type == EventType.ON_RECOGNIZING_SPEECH_FINISHED:
+                user_input = event.args['text']
+                log_file.write("User: " + user_input + "\n")
+                if user_input.lower() == 'bye':
+                    assistant.stop_conversation()
+                    speak("Goodbye!")
+                    log_file.write("Assistant: Goodbye!\n")
+                    break
+                elif user_input.lower().startswith('hey google'):
+                    send_to_zapier(user_input)
+                elif user_input.lower().startswith('chatbot'):
+                    response = chatbot.get_response(user_input[7:])
+                    send_to_telegram(response)
+                    assistant.send_text_query(response)
+                    speak(response)
+                    log_file.write("Assistant: " + str(response) + "\n")
+                elif user_input.lower().startswith('openai'):
+                    response = generate_response(user_input[7:])
+                    send_to_telegram(response)
+                    assistant.send_text_query(response)
+                    speak(response)
+                    log_file.write("Assistant: " + str(response) + "\n")
+                else:
+                    response = chatbot.get_response(user_input)
+                    send_to_telegram(response)
+                    assistant.send_text_query(response)
+                    speak(response)
+                    log_file.write("Assistant: " + str(response) + "\n")
+            elif event.type == EventType.ON_CONVERSATION_TURN_FINISHED:
+                print('Assistant idle')
     log_file.close()
 
 def speak(text):
@@ -66,19 +84,6 @@ def generate_response(prompt):
     )
     message = response.choices[0].text.strip()
     return message
-
-def listen():
-    with sr.Microphone() as source:
-        print("Say something!")
-        audio = r.listen(source)
-        try:
-            text = r.recognize_google(audio)
-            print("You said: " + text)
-            return text
-        except sr.UnknownValueError:
-            print("Google Speech Recognition could not understand audio")
-        except sr.RequestError as e:
-            print("Could not request results from Google Speech Recognition service; {0}".format(e))
 
 def send_to_zapier(message):
     data = {"message": message}
